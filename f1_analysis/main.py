66e9ac7736449653d0538dd7c1636b92f6436498
mrtwolf7 <lupo.tiziano94@gmail.com>
2025-05-04 21:06:13 +0100

    gini, track metrics and season metrics

--- f1_analysis/main.py
+++ f1_analysis/main.py
@@ -8,9 +8,11 @@ from f1_client.ergast_client import (
 from metrics.race_metrics import (
     get_position_changes,
     get_position_interval,
-    convert_to_timedelta_column
+    convert_to_timedelta_column,
+    compute_gini_coefficient
 )
 import pandas as pd
+from collections import Counter
 
 year = 2024
 seasons = get_available_seasons()
@@ -33,25 +35,73 @@ def create_race_df(results):
     
     return df
 
+
+def create_race_metrics_df(df_race, year, track_id, track_name):
+    average_gaps = df_race['time_interval'].iloc[1:].mean()
+    pos_changes = df_race['position_change'].abs().sum()
+    win_driver = df_race['driver'][0]
+    win_constructor = df_race['constructor'][0]
+
+    df_race_metrics = pd.DataFrame([{
+        'year': year,
+        'track_id': track_id,
+        'track_name': track_name,
+        'average_gap': average_gaps,
+        'position_change': pos_changes,
+        'winner': win_driver,
+        'constructor': win_constructor
+    }])
+
+    return df_race_metrics
+
+
+def create_season_metrics_df(year, df_races_metrics):
+    df_season = df_races_metrics.loc[df_races_metrics['year']==year]
+    average_gaps = sum(df_season['average_gap']) / len(df_season)
+    average_position_changes = sum(df_season['position_change']) / len(df_season)
+    winner_drivers_gini = compute_gini_coefficient(list(Counter(df_season['winner']).values()))
+    winner_constructors_gini = compute_gini_coefficient(list(Counter(df_season['constructor']).values()))    
+
+    df_season_metrics = pd.DataFrame([{
+        'year': year,
+        'average_gaps': average_gaps,
+        'average_position_change': average_position_changes,
+        'winner_drivers_gini': winner_drivers_gini,
+        'winner_constructors_gini': winner_constructors_gini
+    }])
+
+    return df_season_metrics
+
+
 def main():
     races = get_season_races(year)
     print(f"Found {len(races)} races in {year}")
 
     race_gaps = []
     position_changes = []
+    winner_drivers = []
+    winner_constructors = []
+
+    df_races_metrics = pd.DataFrame()
 
     for race in races:
+        track_id = race['Circuit']['circuitId']
+        track_name = race['Circuit']['circuitName']
+
         results = get_race_results(year, race['round'])
         df_race = create_race_df(results)
-        print(df_race)
+        df_race_metrics = create_race_metrics_df(df_race, year, track_id, track_name)    
+        df_races_metrics = pd.concat([df_races_metrics, df_race_metrics])
 
-        race_gaps.append(df_race['time_interval'].iloc[1:].mean())
-        position_changes.append(df_race['position_change'].abs().sum())
+    print(df_races_metrics)
 
-    season_average_gaps = sum(race_gaps) / len(race_gaps) if race_gaps else None
-    season_average_position_changes = sum(position_changes) / len(position_changes) if position_changes else None
-    print(season_average_gaps)
-    print(season_average_position_changes)
+    df_season_metrics = create_season_metrics_df(year, df_races_metrics)
+    print(df_season_metrics)
+
+    # TODO diversity of top 5
+    # TODO % point drivers season
+    # TODO % point constructors season
+    # TODO collect and iterate over seasons
 
 
 if __name__ == "__main__":
--- f1_analysis/metrics/race_metrics.py
+++ f1_analysis/metrics/race_metrics.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 def get_position_changes(final_position, grid_position):
     position_change = grid_position-final_position
@@ -27,3 +29,18 @@ def convert_to_timedelta_column(times):
 
 def get_position_interval(time_series):
     return time_series.diff().dt.total_seconds()
+
+
+def compute_gini_coefficient(values):
+    """Compute Gini coefficient of a list of numeric values (e.g. counts).
+        Higher values mean less diversity in the metrics"""
+    array = np.array(values)
+    if np.amin(array) < 0:
+        raise ValueError("Negative values not allowed in Gini calculation.")
+    array = np.sort(array)
+    n = len(array)
+    cum_diffs = np.abs(array[:, None] - array).sum()
+    mean = array.mean()
+    if mean == 0:
+        return 0
+    return 1 - (cum_diffs / (2 * n**2 * mean))
