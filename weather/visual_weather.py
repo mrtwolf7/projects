3b970df7bed6fee279a80fd3cf83110d1c6d4f89
mrtwolf7 <lupo.tiziano94@gmail.com>
2025-06-01 16:17:47 +0100

    Monthly weather trend

--- weather/visual_weather.py
+++ weather/visual_weather.py
@@ -3,6 +3,7 @@ from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 # Load the data
 df = pd.read_csv("weather.csv")
@@ -11,7 +12,7 @@ df = pd.read_csv("weather.csv")
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "Easter Mondays stats"
 
-# Get list of available tracks
+# Get list of available citie
 cities = sorted(df['city'].unique())
 
 # App layout
@@ -29,14 +30,14 @@ app.layout = html.Div([
 
     dbc.Row([
         dbc.Col(dcc.Graph(id='rainy-years'), width=12),
-        #dbc.Col(dcc.Graph(id='rainy-months'), width=6),
+        dbc.Col(dcc.Graph(id='rainy-months'), width=12),
     ])
 ])
 
 # Callbacks
 @app.callback(
     Output('rainy-years', 'figure'),
-    #Output('rainy-months', 'figure'),
+    Output('rainy-months', 'figure'),
     Input('city-dropdown', 'value')
 )
 def update_graphs(selected_city):
@@ -45,17 +46,52 @@ def update_graphs(selected_city):
         dff.groupby(['year', 'easter_monday'], as_index=False).agg({'rain_mm': 'mean'})
     )
 
-    dff_em = dff[dff['easter_monday'] == True]
-    dff_otherdays = dff[dff['easter_monday'] == False]
+    dff['month_day_num'] = pd.to_datetime(dff['day']).dt.strftime('%m%d').astype(int)
+    dff['month_day_str'] = dff['month_day_num'].apply(lambda x: f"{str(x)[:1]}-{str(x)[1:]}")
+    dff_grouped_month = (
+        dff.groupby(['month_day_num', 'month_day_str', 'easter_monday'], as_index=False)
+        .agg({'rain_mm': 'mean'})
+        .sort_values('month_day_num')  # ensures calendar order
+    )
+    df_line = dff_grouped_month[dff_grouped_month['easter_monday'] == False]
+    df_dots = dff_grouped_month[dff_grouped_month['easter_monday'] == True]
 
-    # Bar chart: 
+
+    # Bar chart: year rainy days
     fig_rain_years = px.bar(dff_grouped, x='year', y='rain_mm', title='Rain Over Years', color='easter_monday', barmode='group')
     fig_rain_years.update_traces(textposition='outside')
     fig_rain_years.update_xaxes(tickmode='linear', dtick=1, tickangle=45)
 
+    # Timeseries: month rainy days
+    fig_rain_month_ts = go.Figure()
+    fig_rain_month_ts.add_trace(go.Scatter(
+        x=df_line['month_day_str'],
+        y=df_line['rain_mm'],
+        mode='lines',
+        name='Avg Rain (Non-Easter Monday)'
+    ))
+    fig_rain_month_ts.add_trace(go.Scatter(
+        x=df_dots['month_day_str'],
+        y=df_dots['rain_mm'],
+        mode='markers+text',
+        name='Easter Monday',
+        marker=dict(size=10, color='red'),
+        text=df_dots['rain_mm'],
+        textposition='top center'
+    ))
+    fig_rain_month_ts.update_layout(
+        title='Rain mm Over Days of the Year',
+        xaxis_title='Day (MM-DD)',
+        yaxis_title='Rain (mm)',
+    )
+    fig_rain_month_ts.update_xaxes(
+        tickangle=45,
+        type = 'category'
+        )
 
     return (
-        fig_rain_years
+        fig_rain_years,
+        fig_rain_month_ts
     )
 
 
