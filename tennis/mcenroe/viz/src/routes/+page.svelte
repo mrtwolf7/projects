e9c48a250ea594fd7673d9e516dc8426d8d11eb5
mrtwolf7 <lupo.tiziano94@gmail.com>
2026-02-02 23:00:26 +0000

    [mcenroe] Adding viz

---
+++ tennis/mcenroe/viz/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import SecMatchesWTime from '$lib/sec_matches_w_time.svelte';
+</script>
+
+<article>
+  <h1>Serve like McEnroe</h1>
+
+  <SecMatchesWTime />
+
+</article>
+
+<style>
+  article {
+    max-width: 960px;
+    margin: 3rem auto;
+    padding: 0 1rem;
+  }
+
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    line-height: 1.6;
+    margin-bottom: 1rem;
+    color: #444;
+  }
+</style>
