6ec56fda46b3bf3bd9bd088b797a1322f3efd9f5
mrtwolf7 <lupo.tiziano94@gmail.com>
2026-02-15 19:34:30 +0000

    update apps portgolio

---
+++ apps/portfolio/src/lib/data/projects.ts
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: "F1 Analysis",
+    slug: "f1-analysis",
+    description:
+      "What are the most predictable F1 races? Has it change over time? Check this analysis with Dash.",
+    demoUrl: "#",
+    repoUrl: "https://github.com/mrtwolf7/projects/tree/main/f1_analysis",
+    type: "dash",
+    stack: ["Dash", "Pandas"]
+  },
+  {
+    title: "Weather data explorer",
+    slug: "weather-analysis",
+    description:
+      "Does it always rain on Easter Monday in Rome? Is it worse than other italian cities?",
+    demoUrl: "#",
+    repoUrl: "https://github.com/mrtwolf7/projects/tree/main/weather",
+    type: "dash",
+    stack: ["Dash", "Pandas"]
+  },
+  {
+    title: "Tennis predictability",
+    slug: "weather-analysis",
+    description:
+      "Ah, tennis nowadays is so predictable. How true is that?",
+    demoUrl: "#",
+    repoUrl: "https://github.com/mrtwolf7/projects/tree/main/tennis",
+    type: "svelte",
+    stack: ["Svelte", "D3", "Pandas"]
+  }  
+];
